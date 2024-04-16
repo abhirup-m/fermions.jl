bc872b614396feadd35959f96807258891e0585e
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-04-16 11:13:45 +0530

    added parameter descriptions in function

--- source/models.jl
+++ source/models.jl
@@ -1,14 +1,22 @@
 include("fermionise.jl")
 
 function KondoKSpace(basisStates, numBathStates, couplings)
+    # Hamiltonian is of the form
+    # H = \sum_k ε_k n̂_k + \sum_{k1, k2} J_{k1, k2} S_d ⋅σ_{α,β} c^†_{k1 α} c_{k2 β}
+    #     - 0.5\sum_{1,2,3,4} W_{1,2,3,4} \sum_σ (c^†_{1 σ} c_{2 σ}c^†_{3 σ} c_{4 σ} 
+    #     - c^†_{1 σ} c_{2 σ}c^†_{3 σ̄} c_{4 σ̄})
+    # Various sites and states are labelled as
     # imup    imdn    k1up    k1dn    k2up    k2dn    k3up    k3dn ...
     # 0       1       2       3       4       5       6       7    ...
 
-    if length(basisStates) == 0
-        println("Chosen basis is empty, bailing out.")
-        return
-    end
+    # We need the following couplings:
+    # dispersion_arr: 1D vector of kinetic energies for the required momentum states
+    # kondoCouplingMatrix: 2D matrix with matrix elements J[i,j] = J_{ki, kj}
+    # bathIntFunc: Dictionary that defines the 4-point bath interaction terms. The keys
+    # are quartets of momentum indices [i,j,p,q], and the value at the key is the 
+    # interaction strength W_{ki, kj, kp, kq}.
     (dispersion_arr, kondoCouplingMatrix, bathIntFunc) = couplings
+
     @assert length(dispersion_arr) == numBathStates
     @assert size(kondoCouplingMatrix) == (numBathStates, numBathStates)
 
@@ -56,13 +64,13 @@ function KondoKSpace(basisStates, numBathStates, couplings)
         downIndices = 2 .* momIndices .+ 2
 
         # -0.5 W_{1,2,3,4} c^†_{1 ↑} c_{2, ↑} c^†_{3 ↑} c_{4, ↑}
-        push!(operatorList, ("+-+-", -0.5 * bathIntFunc[sort(collect(momIndices))], upIndices))
+        push!(operatorList, ("+-+-", -0.5 * bathIntFunc[collect(momIndices)], upIndices))
 
         # -0.5 W_{1,2,3,4} c^†_{1 ↓} c_{2, ↓} c^†_{3 ↓} c_{4, ↓}
-        push!(operatorList, ("+-+-", -0.5 * bathIntFunc[sort(collect(momIndices))], downIndices))
+        push!(operatorList, ("+-+-", -0.5 * bathIntFunc[collect(momIndices)], downIndices))
 
         # W_{1,2,3,4} c^†_{1 ↑} c_{2, ↑} c^†_{3 ↓} c_{4, ↓}
-        push!(operatorList, ("+-+-", bathIntFunc[sort(collect(momIndices))], [upIndices[1:2]; downIndices[3:4]]))
+        push!(operatorList, ("+-+-", bathIntFunc[collect(momIndices)], [upIndices[1:2]; downIndices[3:4]]))
 
     return generalOperatorMatrix(basisStates, operatorList)
 end
