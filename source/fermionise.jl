11efc45b7e9c3908278248b5215bee369fc96f06
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-04-19 12:43:29 +0530

    updated operator matrix generator

--- source/fermionise.jl
+++ source/fermionise.jl
@@ -1,7 +1,10 @@
-using Combinatorics
+using LinearAlgebra
 using Distributed
-using ProgressMeter
-using SparseArrays
+addprocs(1)
+@everywhere using Combinatorics
+@everywhere using ProgressMeter
+# @everywhere using Term.Progress
+@everywhere using OrderedCollections
 
 
 """
@@ -18,39 +21,39 @@ Optionally accepts a parameter totOccupancy that restricts the basis states to o
 those with the specified total occupancy, and a parameter totSpin that does the same
 but for a specified total magnetisation.
 """
-function BasisStates(numLevels::Int64; totOccupancy::Int64=-999, totSpin::Float64=-999.0)
+function BasisStates(numLevels::Int64; totOccupancy::Union{Vector{Int64},Nothing}=nothing)
 
     # if totOccupancy is not set, all occupancies from 0 to N are allowed,
     # otherwise use only the provided totOccupancy
-    if totOccupancy == -999
+    if isnothing(totOccupancy)
         allowedOccupancies = 0:numLevels
     else
-        @assert 0 <= totOccupancy <= numLevels
-        allowedOccupancies = (totOccupancy,)
+        @assert all(0 .<= totOccupancy .<= numLevels)
+        allowedOccupancies = totOccupancy
     end
 
     # create dictionary to store configurations classified by their total occupancies.
     # For eg, basisStates = {0: [[0, 0]], 1: [[1, 0], [0, 1]], 2: [[1, 1]]}}
-    basisStates = Dict((totOcc => BitArray[] for totOcc in allowedOccupancies))
+    basisStates = OrderedDict((totOcc => BitArray[] for totOcc in allowedOccupancies))
 
     # generate all possible configs, by running integers from 0 to 2^N-1, and converting
     # them to binary forms
-    allConfigs = digits.(0:2^numLevels - 1, base=2, pad=numLevels) |> reverse
+    allConfigs = digits.(0:2^numLevels-1, base=2, pad=numLevels) |> reverse
 
     # loop over allowed occupancies, and store the configs in the dict
-    # classified by their total occupancies.
-    Threads.@threads for totOcc in allowedOccupancies
-        basisStates[totOcc] = allConfigs[sum.(allConfigs) .== totOcc]
+    # classified by their total occupancies. We will also store the subdimensions
+    # while we are creating the dictionary.
+    subDimensions = Vector{Int64}(undef, length(allowedOccupancies))
+    Threads.@threads for index in eachindex(allowedOccupancies)
+        totOcc = allowedOccupancies[index]
+        basisStates[totOcc] = allConfigs[sum.(allConfigs).==totOcc]
+        subDimensions[index] = length(basisStates[totOcc])
     end
 
-    return basisStates
+    return basisStates, subDimensions
 end
 
-
-"""
-Transforms a single Fock state |n> (n=0/1) given an operator (= c/c^†/n̂/1-n̂).
-"""
-function TransformBit(qubit::Int64, operator::Char)
+@everywhere function TransformBit(qubit::Bool, operator::Char)
     if operator == 'n'
         return qubit, qubit
     elseif operator == 'h'
@@ -62,44 +65,67 @@ function TransformBit(qubit::Int64, operator::Char)
     end
 end
 
+function applyOperatorOnState(stateDict::Dict{BitVector,Float64}, operatorList::Vector{Tuple{String,Float64,Vector{Int64}}})
+    # define a dictionary for the final state obtained after applying 
+    # the operators on all basis states
+    completeOutputState = Dict{BitVector,Float64}()
 
-function applyOperatorOnState(stateDict::Dict{Vector{Int64},Float64}, operatorList::Vector{Tuple{String,Float64,Vector{Int64}}})
-    completeOutputState = @distributed (a, b) -> merge(+, a, b) for (opType, opStrength, opMembers) in operatorList
-        outputState = Dict()
+    # loop over all operator tuples within operatorList
+    for (opType, opStrength, opMembers) in operatorList
+        @assert unique(opMembers) == opMembers
+
+        holeProjectionSites = opMembers[findall(x -> x == '+' || x == 'h', opType)]
+        particleProjectionSites = opMembers[findall(x -> x == '-' || x == 'n', opType)]
+
         for (state, coefficient) in stateDict
+
+            # from the outset, ignore states that are creating at occupied bits, destroying 
+            # at empty bits, applying n at empty bits or checking 1-n at occupied bits.
+            if 1 in state[holeProjectionSites] || 0 in state[particleProjectionSites]
+                continue
+            end
+
             newCoefficient = coefficient
             newState = copy(state)
+
+            # for each basis state, obtain a modified state after applying the operator tuple
             for (siteIndex, operator) in zip(reverse(opMembers), reverse(opType))
                 newQubit, factor = TransformBit(newState[siteIndex], operator)
                 if factor == 0
                     newCoefficient = 0
                     break
                 end
-                newState[siteIndex] = newQubit
-                exchangeSign = operator in ['+', '-'] ? (-1)^sum(newState[1:siteIndex]) : 1
+                # calculate the fermionic exchange sign by counting the number of
+                # occupied states the operator has to "hop" over
+                exchangeSign = operator in ['+', '-'] ? (-1)^sum(newState[1:siteIndex-1]) : 1
+                @inbounds newState[siteIndex] = newQubit
                 newCoefficient *= exchangeSign * factor
             end
+
+            # if the coefficient of the transformation is non-zero, add this to the output state
+            # dictionary for the operator tuple we are currently looping over
             if newCoefficient != 0
-                if newState in keys(outputState)
-                    outputState[newState] += opStrength * newCoefficient
+                if newState in keys(completeOutputState)
+                    completeOutputState[newState] += opStrength * newCoefficient
                 else
-                    outputState[newState] = opStrength * newCoefficient
+                    completeOutputState[newState] = opStrength * newCoefficient
                 end
             end
         end
-        outputState
     end
     return completeOutputState
 end
 
-function generalOperatorMatrix(basisStates::Vector{Vector{Int64}}, operatorList::Vector{Tuple{String,Float64,Vector{Int64}}})
-    operatorDimension = length(basisStates)
-    operatorMatrix = zeros((operatorDimension, operatorDimension))
-    Threads.@threads for (index1, state) in collect(enumerate(basisStates))
-        stateDict = Dict(state => 1.0)
-        for (newState, coefficient) in applyOperatorOnState(stateDict, operatorList)
-            operatorMatrix[index1, basisStates.==[newState]] .= coefficient
+function generalOperatorMatrix(basisStates::OrderedDict{Int64,Vector{BitArray}}, operatorList::Vector{Tuple{String,Float64,Vector{Int64}}})
+    operatorFullMatrix = OrderedDict{Int64,Matrix{Float64}}()
+    Threads.@threads for (totOcc, bstates) in collect(pairs(basisStates))
+        operatorSubMatrix = zeros(length(bstates), length(bstates))
+        Threads.@threads for (index, state) in collect(enumerate(bstates))
+            newState = applyOperatorOnState(Dict(state => 1.0), operatorList)
+            matchingIndices = findall(in(keys(newState)),bstates)
+            @inbounds operatorSubMatrix[index, matchingIndices] .= collect(values(newState)) 
         end
+        operatorFullMatrix[totOcc] = operatorSubMatrix
     end
-    return operatorMatrix
+    return operatorFullMatrix
 end
