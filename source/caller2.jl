67d99edf14f832a9f58351b09e23afb3600c3dba
abhirup-m <mukherjeeabhirup44@gmail.com>
2024-05-17 23:38:22 +0530

    converted into jl package

--- source/caller2.jl
+++
@@ -1,7 +0,0 @@
-include("fermionise.jl")
-len = 18
-basis = BasisStates(len, totOccupancy=[trunc(Int, len / 2)])
-ham_op_list = [("nn", 1.0, [i, i + 1]) for i in 1:len-1]
-H = generalOperatorMatrix(basis, oplist)
-eigvals, eigstates = getSpectrum(H)
-gstateCorrelation(basis, eigvals, eigstates, oplist)
---
+++ src/fermions.jl
@@ -0,0 +1,7 @@
+module fermions
+
+include("base.jl")
+include("models.jl")
+include("correlations.jl")
+
+end # module fermions
