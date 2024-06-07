0ac51ee318184fe7d4be2c18b6541790bc458034
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-06-07 15:31:02 +0530

    added density matrix

--- src/eigen.jl
+++ src/eigen.jl
@@ -25,7 +25,7 @@ end
 function getGstate(eigvals::Dict{Tuple{Int64,Int64},Vector{Float64}}, eigvecs::Dict{Tuple{Int64,Int64},Vector{Vector{Float64}}})
     gsEnergy = minimum(minimum.(values(eigvals)))
     groundStates = Vector{Float64}[]
-    blocks = []
+    blocks = keytype(eigvals)[]
     for (block, eigvalsBlock) in eigvals
         for (i, _) in enumerate(eigvalsBlock[eigvalsBlock .≈ gsEnergy])
             push!(groundStates, eigvecs[block][i])
@@ -35,3 +35,23 @@ function getGstate(eigvals::Dict{Tuple{Int64,Int64},Vector{Float64}}, eigvecs::D
     return gsEnergy, groundStates, blocks
 end
 
+
+function reducedDM(basisStates::Vector{BitArray}, groundState::Vector{Float64}, reducingIndices::Vector{Int64}; reducingConfigs::Vector{BitVector}=BitVector[])
+    nonReducingIndices = setdiff(1:length(basisStates[1]), reducingIndices)
+    if length(reducingConfigs) == 0
+        reducingConfigs = [collect(config) for config in Iterators.product(fill([1, 0], length(reducingIndices))...)]
+    end
+    nonReducingConfigs = vec(collect(Iterators.product(fill([1, 0], length(nonReducingIndices))...)))
+    reducingCoeffs = Dict{BitVector, Dict{BitVector, Float64}}(BitVector(config) => Dict(BitVector(configPrime) => 0.0 
+                                                                        for configPrime in nonReducingConfigs) for config in reducingConfigs)
+    for (state, coeff) in zip(basisStates, groundState)
+        if state[reducingIndices] ∈ reducingConfigs
+            reducingCoeffs[state[reducingIndices]][state[nonReducingIndices]] += coeff
+        end
+    end
+    reducedDMatrix = zeros(length(reducingConfigs), length(reducingConfigs))
+    for ((i1, c1), (i2, c2)) in Iterators.product(enumerate(reducingConfigs), enumerate(reducingConfigs))
+        reducedDMatrix[i1, i2] = sum(values(merge(*, reducingCoeffs[collect(c1)], reducingCoeffs[collect(c2)])))
+    end
+    return reducedDMatrix
+end
