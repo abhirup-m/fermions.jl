c54b2287ad8e5837163a029ddbb53bc41282ba83
abhirup-m <mukherjeeabhirup44@gmail.com>
2024-06-10 11:23:20 +0530

    added entanglement measures

--- src/correlations.jl
+++ src/correlations.jl
@@ -8,3 +8,26 @@ function gstateCorrelation(basisStates::Dict{Tuple{Int64,Int64},Vector{BitArray}
     end
     return correlationResults
 end
+
+
+function vnEntropy(basisStates::Vector{BitArray}, groundState::Vector{Float64}, reducingIndices::Vector{Int64}; reducingConfigs::Vector{BitVector}=BitVector[])
+    reducedDMatrix = reducedDM(basisStates, groundState, reducingIndices; reducingConfigs=reducingConfigs)
+    eigenvalues = eigvals(Hermitian(reducedDMatrix))
+    eigenvalues[eigenvalues .< 1e-16] .= 0
+    @assert all(x -> x ≥ 0, eigenvalues)
+    return -sum(eigenvalues .* log.(replace(eigenvalues, 0 => 1e-10)))
+end
+
+
+function mutInfo(basisStates::Vector{BitArray}, 
+        groundState::Vector{Float64}, 
+        reducingIndices::Tuple{Vector{Int64}, Vector{Int64}};
+        reducingConfigs::Tuple{Vector{BitVector}, Vector{BitVector}}=(BitVector[], BitVector[])
+    )
+    combinedConfigs = vec([[c1; c2] for (c1, c2) in Iterators.product(reducingConfigs...)])
+    SEE_A = vnEntropy(basisStates, groundState, reducingIndices[1]; reducingConfigs=reducingConfigs[1])
+    SEE_B = vnEntropy(basisStates, groundState, reducingIndices[2]; reducingConfigs=reducingConfigs[2])
+    SEE_AB = vnEntropy(basisStates, groundState, vcat(reducingIndices...); reducingConfigs=combinedConfigs)
+    println((SEE_A, SEE_B, SEE_AB))
+    return SEE_A + SEE_B - SEE_AB
+end
