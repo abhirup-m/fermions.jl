8caaacfbff563e0344fd2f465701ed74364353af
abhirup-m <mukherjeeabhirup44@gmail.com>
2024-07-01 01:59:39 +0530

    redesigned stuff with filling

--- src/iterativeDiag.jl
+++ src/iterativeDiag.jl
@@ -1,13 +1,14 @@
 """Expands the basis to accomodate new 1-particle states by tacking 
 product states on to existing states. For eg. |10> + |01> -> (|10> + |01>)⊗|1>.
 """
-function expandBasis(basisStates::Dict{Tuple{Int64, Int64}, Vector{Dict{BitVector, Float64}}}, 
+function expandBasis(
+        basisStates::Dict{Tuple{Float64, Int64}, Vector{Dict{BitVector, Float64}}}, 
         numAdditionalSites::Int64,
-        energyVals::Dict{Tuple{Int64, Int64}, Vector{Float64}},
+        energyVals::Dict{Tuple{Float64, Int64}, Vector{Float64}},
         insertPosition::Int64,
         retainSize::Int64;
-        totOccReq::Vector{Int64}=[],
-        totSzReq::Vector{Int64}=[]
+        totOccReq::Vector{Float64}=Float64[],
+        totSzReq::Vector{Int64}=[1, 0, -1]
     )
     @assert numAdditionalSites > 0
 
@@ -24,7 +25,7 @@ function expandBasis(basisStates::Dict{Tuple{Int64, Int64}, Vector{Dict{BitVecto
     # two purposes:(i) sorts the full set of new basis states so that we can 
     # take the M least energetic states (truncation), and (ii) these energy
     # values form the diagonal elements of the new Nth Hamiltonian.
-    diagElements = Dict{Tuple{Int64, Int64}, Vector{Float64}}()
+    diagElements = Dict{Tuple{Float64, Int64}, Vector{Float64}}()
 
     # loop over all combinations of the new states that are being added. if only
     # one states is being added, then 0 and 1. if two, then 00, 01, 10 and 11.
@@ -33,27 +34,24 @@ function expandBasis(basisStates::Dict{Tuple{Int64, Int64}, Vector{Dict{BitVecto
         # check the occupancy and Sz associated with these combinations
         extraOcc = sum(newBitCombination)
         extraSz = sum(newBitCombination[1:2:end]) - sum(newBitCombination[2:2:end])
-
         # loop over the existing old basis states
         Threads.@threads for ((totOcc, totSz), basisArr) in collect(basisStates)
 
             # only consider further if the total new occupancy and total new Sz
             # (after attaching the extra states with the old state) fall in the 
             # range that are required (the required range is passed as function arg.)
-            avgOcc = 2 * totOcc / length.(keys(basisArr[1]))[1] + 2 * extraOcc / length(newBitCombination)
+            avgOcc = (totOcc * length.(keys(basisArr[1]))[1] + extraOcc) / (length.(keys(basisArr[1]))[1] + length(newBitCombination))
             if (!isempty(totOccReq) && avgOcc ∉ totOccReq) || (!isempty(totSzReq) && (totSz + extraSz) ∉ totSzReq)
                 continue
             end
 
-            newKey = (totOcc + extraOcc, totSz + extraSz)
+            newKey = (avgOcc, totSz + extraSz)
 
             # loop over the computational basis components of the old state
             for (energy, basisStateDict) in zip(energyVals[(totOcc, totSz)], basisArr)
-
                 # form new basis states by joining the new and the old 
                 # computational states: |10> -><- |0> = |100>
-                expandedBasisStates = [vcat(key[1:insertPosition-1], collect(newBitCombination), 
-                                            key[insertPosition:end])
+                expandedBasisStates = [vcat(key, collect(newBitCombination))
                                        for key in keys(basisStateDict)]
 
                 if newKey ∉ keys(newBasisStates)
@@ -80,35 +78,37 @@ end
 
 """Performs a rotation of the given basis according to the given transformation. 
 Can be used to, for eg., transfer into an eigen basis."""
-function transformBasis(basisStates::Dict{Tuple{Int64, Int64}, Vector{Dict{BitVector, Float64}}},
-        transformation::Dict{Tuple{Int64, Int64}, Vector{Vector{Float64}}})
+function transformBasis(
+        basisStates::Dict{Tuple{Float64, Int64}, Vector{Dict{BitVector, Float64}}},
+        transformation::Dict{Tuple{Float64, Int64}, Vector{Vector{Float64}}})
 
     # check that the symmetry sectors of the transformation 
     # are part of the old basis
     @assert issubset(keys(transformation), keys(basisStates))
 
     # stores the new rotated basis
-    newBasisStates = Dict{keytype(basisStates), valtype(basisStates)}(k => [Dict() for vi in v] for (k, v) in transformation)
+    newBasisStates = Dict{keytype(basisStates), valtype(basisStates)}()
 
     # loop over symmetry sectors of the transformation
     Threads.@threads for (sector, vectors) in collect(transformation)
 
-        # loop over the eigen vectors of the transformation
-        # (for eg. eigenvector = [-0.5, 0.5, 0.5, 0.5])
-        Threads.@threads for (i, eigenvector) in collect(enumerate(vectors))
+        newBasisStates[sector] = rotateStates(basisStates[sector], vectors)
+        # # loop over the eigenvectors of the transformation
+        # # (for eg. eigenvector = [-0.5, 0.5, 0.5, 0.5])
+        # Threads.@threads for (i, eigenvector) in collect(enumerate(vectors))
 
-            # loop over the v_i^j
-            for (j, multiplier) in enumerate(eigenvector)
-                if multiplier == 0
-                    continue
-                end
+        #     # loop over the v_i^j
+        #     for (j, multiplier) in enumerate(eigenvector)
+        #         if multiplier == 0
+        #             continue
+        #         end
 
-                # create the new basis states: {|1>: v_i^1 * b_{1, old}, |2>: v_i^2 * b_{2, old} ... }
-                multipliedState = Dict(q => multiplier * b for (q, b) in basisStates[sector][j])
-                mergewith!(+, newBasisStates[sector][i], multipliedState)
-            end
-            @assert !isempty(newBasisStates[sector][i])
-        end
+        #         # create the new basis states: {|1>: v_i^1 * b_{1, old}, |2>: v_i^2 * b_{2, old} ... }
+        #         multipliedState = Dict(q => multiplier * b for (q, b) in basisStates[sector][j])
+        #         mergewith!(+, newBasisStates[sector][i], multipliedState)
+        #     end
+        #     @assert !isempty(newBasisStates[sector][i])
+        # end
     end
     return newBasisStates
 end
@@ -122,11 +122,11 @@ the basis of these Ns states, then diagonalises it, etc.
 """
 function iterativeDiagonaliser(
         hamiltonianFamily::Vector{Dict{Tuple{String,Vector{Int64}},Float64}},
-        initBasis::Dict{Tuple{Int64, Int64}, Vector{Dict{BitVector, Float64}}},
+        initBasis::Dict{Tuple{Float64, Int64}, Vector{Dict{BitVector, Float64}}},
         numStatesFamily::Vector{Int64},
         insertPosition::Vector{Int64},
         retainSize::Int64;
-        occSubspace::Vector{Int64}=[1],
+        occSubspace::Vector{Float64}=Float64[],
         SzSubspace::Vector{Int64}=[1, 0, -1],
         tolerance::Float64=1e-16
     )
@@ -139,7 +139,7 @@ function iterativeDiagonaliser(
     basisStates = initBasis
 
     # stores the current eigenvalues
-    diagElements = Dict{Tuple{Int64, Int64}, Vector{Float64}}(k => zeros(length(v)) for (k, v) in basisStates)
+    diagElements = Dict{Tuple{Float64, Int64}, Vector{Float64}}(k => zeros(length(v)) for (k, v) in basisStates)
 
     # loop over the Hamiltonians H_N.
     @showprogress for (i, hamiltonian) in enumerate(hamiltonianFamily)
@@ -160,12 +160,13 @@ function iterativeDiagonaliser(
 
         # diagonalise this matrix, keeping only 'retainSize' 
         # number of the new eigenstates.
-        spectrum = getSpectrum(hamiltonianMatrix; maxNum=retainSize, tolerance=tolerance)
+        spectrum = getSpectrum(basisStates, hamiltonianMatrix; maxNum=retainSize, tolerance=tolerance)
+
         push!(spectrumFamily, spectrum)
         @assert keys(basisStates) == keys(spectrum[1]) == keys(spectrum[2])
 
         # create basis out of the eigenstates
-        basisStates = transformBasis(basisStates, spectrum[2])
+        basisStates = spectrum[2]#transformBasis(basisStates, spectrum[2])
         if i < length(numStatesFamily)
             # expand the new basis to accomodate the new sites for the next step.
             basisStates, diagElements = expandBasis(basisStates, numStatesFamily[i+1] - numStatesFamily[i], spectrum[1], insertPosition[i], retainSize; totOccReq=occSubspace, totSzReq=SzSubspace)
