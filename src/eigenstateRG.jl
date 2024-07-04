dc35089d6898f00abc1ee7e59ba4e482fda1f9f5
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-07-04 17:10:23 +0530

    simplified eigenstate RG file

--- src/eigenstateRG.jl
+++ src/eigenstateRG.jl
@@ -1,44 +1,13 @@
-function InitWavefunction(basisStates::Vector{BitVector}, initCouplings, hamiltonianFunction; tolerance=1e-10)
-    operatorList = hamiltonianFunction(initCouplings...)
-    hamiltonianMatrix = operatorMatrix(basisStates, operatorList)
-    groundStates = getGstate(hamiltonianMatrix)
-    @assert length(groundStates) == 1
-    stateDict = Dict(bstate => coeff for (bstate,coeff) in zip(basisStates, groundStates[1]) if abs(coeff) > tolerance)
-    return stateDict
-end
-
-
-function ApplyInverseTransform(state, unitaryOperatorFunction, alpha, sectors)
-    @assert sectors in ["p", "h", "ph"] "Provided IOM sectors not among p, h or ph/hp."
-    numEntangled = trunc(Int, length(collect(keys(state))[1]) / 2)
-    if sectors == "p"
-        state = Dict{keytype(state), valtype(state)}([key; [1, 1]] => val for (key, val) in state)
-    elseif sectors == "h"
-        state = Dict{keytype(state), valtype(state)}([key; [0, 0]] => val for (key, val) in state)
-    else
-        state = Dict{keytype(state), valtype(state)}([key; [1, 1, 0, 0]] => val for (key, val) in state)
-    end
-    unitaryOperatorList = unitaryOperatorFunction(alpha, numEntangled, sectors)
-    stateRenormalisation = fetch.([Threads.@spawn applyOperatorOnState(state, Dict(k => v)) for (k,v) in unitaryOperatorList])
-    mergewith!(+, state, stateRenormalisation...)
-
-    total_norm = sum(values(state) .^ 2)^0.5
-    map!(x -> x / total_norm, values(state))
-
-    return state
-end
-
-function getWavefunctionRG(basisStates::Dict{BitVector, Float64}, initCouplings, numEntangledSites::Integer, numReverseSteps::Integer, hamiltonianFunction, unitaryOperatorFunction, stateExpansionFunction, sectors::String, displayGstate=false)
+function getWavefunctionRG(initState::Dict{BitVector, Float64}, initCouplings, numEntangledSites::Integer, numReverseSteps::Integer, hamiltonianFunction, unitaryOperatorFunction, stateExpansionFunction, sectors::String, displayGstate=false)
     
-    initState = InitWavefunction(basisStates, initCouplings, hamiltonianFunction)
     stateFlowArray = Dict{BitVector, Float64}[]
     push!(stateFlowArray, initState)
 
     for step in 1:numReverseSteps
-        newState = stateExpansionFunction(stateFlowArray[end]) #ApplyInverseTransform(stateFlowArray[end], unitaryOperatorFunction, alpha, sectors)
+        newState = stateExpansionFunction(stateFlowArray[end])
         unitaryOperatorList = unitaryOperatorFunction(step)
         stateRenormalisation = fetch.([Threads.@spawn applyOperatorOnState(newState, Dict(k => v)) for (k,v) in unitaryOperatorList])
-        mergewith!(+, newState, newStateRenormalisation...)
+        mergewith!(+, newState, stateRenormalisation...)
 
         total_norm = sum(values(newState) .^ 2)^0.5
         map!(x -> x / total_norm, values(newState))
