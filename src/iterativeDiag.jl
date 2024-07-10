c66802ad1b2a28e59330f6009ccaed841b61ee05
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-07-10 23:33:51 +0530

    completed iterDiag, seems to be 3 times faster than the earlier implementation, needs more benchmarks and unittests

--- src/iterativeDiag.jl
+++ src/iterativeDiag.jl
@@ -1,36 +1,45 @@
 using IterTools
+include("base.jl")
+include("eigen.jl")
 
 """Expands the basis to accomodate new 1-particle states by tacking 
 product states on to existing states. For eg. |10> + |01> -> (|10> + |01>)⊗|1>.
 """
 function ExpandBasis(
-        basis::Vector{Dict{BitVector, Float64}}, 
-        sector::Tuple{Int64, Int64}, 
-        eigvals::Vector{Float64}, 
-        numNew::Int64,
-    )
+    basis::Vector{Dict{BitVector,Float64}},
+    sector::Tuple{Int64,Int64},
+    eigvals::Vector{Float64},
+    numNew::Int64;
+    newClassifiedBasis::Dict{Tuple{Int64,Int64},Vector{Dict{BitVector,Float64}}}=Dict{Tuple{Int64,Int64},Vector{Dict{BitVector,Float64}}}(),
+    newDiagElementsClassified::Dict{Tuple{Int64,Int64},Vector{Float64}}=Dict{Tuple{Int64,Int64},Vector{Float64}}(),
+)
     @assert length(basis) == length(eigvals)
-    expandedBasis = Dict{typeof(sector), typeof(basis)}()
-    diagElements = Dict{typeof(sector), Vector{Float64}}()
-
-    Threads.@threads for newComb in collect(product(repeat([[0, 1]], 2*numNew)...))
-        newSector = sector .+ (sum(newComb), 
-                               sum(newComb[1:2:end]) - sum(newComb[2:2:end]))
-        expandedBasis[newSector] = [Dict(vcat.(keys(stateDict), newComb) .=> values(stateDict)) for stateDict in basis]
-        diagElements[newSector] = eigvals
+    newPairs = []
+    for newComb in collect(IterTools.product(repeat([[0, 1]], 2 * numNew)...))
+        newSector = sector .+ (sum(newComb),
+            sum(newComb[1:2:end]) - sum(newComb[2:2:end]))
+        if newSector ∉ keys(newClassifiedBasis)
+            newClassifiedBasis[newSector] = []
+            newDiagElementsClassified[newSector] = []
+        end
+        push!(newPairs, (newComb, newSector))
     end
-    return expandedBasis, diagElements
+    Threads.@threads for (newComb, newSector) in newPairs
+        append!(newClassifiedBasis[newSector], [Dict([vcat(key, collect(newComb)) for key in keys(stateDict)] .=> values(stateDict)) for stateDict in basis])
+        append!(newDiagElementsClassified[newSector], eigvals)
+    end
+    return newClassifiedBasis, newDiagElementsClassified
 end
 
 
-function ClassifyBasis(basisStates)
-    classifiedBasis = Dict{Tuple{Int64, Int64}, typeof(basisStates)}()
+function ClassifyBasis(basisStates::Vector{Dict{BitVector,Float64}})
+    classifiedBasis = Dict{Tuple{Int64,Int64},typeof(basisStates)}()
 
     # Multhreading doesn't help here, already tried!
     for stateDict in basisStates
         bstate = collect(keys(stateDict))[1]
         totOcc = sum(bstate)
-        totMagzArr = sum(@view bstate[1:2:end]) - sum(@view bstate[2:2:end])
+        totMagzArr = sum(bstate[1:2:end]) - sum(bstate[2:2:end])
         if (totOcc, totMagzArr) ∉ keys(classifiedBasis)
             classifiedBasis[(totOcc, totMagzArr)] = []
         end
@@ -41,66 +50,60 @@ function ClassifyBasis(basisStates)
 end
 
 
+function TruncateBasis(newClassifiedBasis, newDiagElementsClassified, retainSize)
+    classifiedBasis = typeof(newClassifiedBasis)()
+    diagElementsClassified = Dict{keytype(classifiedBasis),Vector{Float64}}()
+    for (sector, basis) in newClassifiedBasis
+        sortperms = sortperm(newDiagElementsClassified[sector])
+        classifiedBasis[sector] = basis[sortperms][1:minimum((retainSize, length(basis)))]
+        diagElementsClassified[sector] = sort(newDiagElementsClassified[sector])[1:minimum((retainSize, length(newDiagElementsClassified[sector])))]
+    end
+    return classifiedBasis, diagElementsClassified
+end
+
+
 """Main function for iterative diagonalisation. Gives the approximate low-energy
 spectrum of a hamiltonian through the following algorithm: first diagonalises a
 Hamiltonian with few degrees of freedom, retains a fixed Ns number of low-energy
 eigenstates, writes the Hamiltonian for a larger number of degrees of freedom in
 the basis of these Ns states, then diagonalises it, etc.
 """
-function iterativeDiagonaliser(
-        hamiltonianFamily::Vector{Dict{Tuple{String,Vector{Int64}},Float64}},
-        initBasis::Dict{Tuple{Int64, Int64}, Vector{Dict{BitVector, Float64}}},
-        numStatesFamily::Vector{Int64},
-        retainSize::Int64;
-        occCriteria::Function = (x,y) -> true,
-        magzCriteria::Function = x -> true,
-        keepfrom::String="IR",
-        tolerance::Float64=1e-12
-    )
-    @assert length(hamiltonianFamily) == length(numStatesFamily)
+function IterDiag(
+    hamFlow::Vector{Vector{Tuple{String,Vector{Int64},Float64}}},
+    basisStates::Vector{Dict{BitVector,Float64}},
+    numSitesFlow::Vector{Int64},
+    retainSize::Int64;
+    occupSectors::Function=(x, N) -> true
+)
+    @assert length(numSitesFlow) == length(hamFlow)
 
-    # stores flow of the spectrum
-    spectrumFamily = []
+    classifiedBasis = ClassifyBasis(basisStates)
+    diagElementsClassified = Dict(k => zeros(length(v)) for (k, v) in classifiedBasis)
 
-    # stores the flowing basis states
-    basisStates = initBasis
+    numDiffFlow = [n2 - n1 for (n1, n2) in zip(numSitesFlow[1:end-1], numSitesFlow[2:end])]
+    eigvalFlow = [Dict() for _ in hamFlow]
+    eigvecFlow = [Dict() for _ in hamFlow]
 
-    # stores the current eigenvalues
-    diagElements = Dict{Tuple{Int64, Int64}, Vector{Float64}}(k => zeros(length(v)) for (k, v) in basisStates)
-
-    # loop over the Hamiltonians H_N.
-    @showprogress desc="iterative diag." for (i, hamiltonian) in enumerate(hamiltonianFamily)
-
-        # at Nth step on bringing sites 'q' into the Hamiltonian,
-        # full hamiltonian H_N is H_N = H_{N-1} + H_{N-1,q} + H_q,
-        # where H_q is the terms acting purely on q, while H_{N-1,q}
-        # is the set of terms that couple 'q' with the N-1 states.
-
-        println("$i. Creating matrix.")
-        # matrix for the H_{N-1,q} + H_q part.
-        hamiltonianMatrix = operatorMatrix(basisStates, hamiltonian)
-        # Adds the matrix elements for H_{N-1}. Since the present basis
-        # is the eigenbasis of H_{N-1}, these matrix elements are the diagonal ones.
-        for k in keys(hamiltonianMatrix)
-            hamiltonianMatrix[k] .+= diagm(diagElements[k])
+    for (i, hamiltonian) in enumerate(hamFlow)
+        newClassifiedBasis = typeof(classifiedBasis)()
+        newDiagElementsClassified = Dict{keytype(classifiedBasis),Vector{Float64}}()
+        spectrum = fetch.([Threads.@spawn Spectrum(hamiltonian, basis, diagElements=diagElementsClassified[sector]) for (sector, basis) in classifiedBasis])
+        for (sector, spectrumSector) in zip(keys(classifiedBasis), spectrum)
+            eigvalFlow[i][sector], eigvecFlow[i][sector] = spectrumSector
+        end
+        if i == length(hamFlow)
+            continue
+        end
+        for sector in keys(classifiedBasis)
+            newClassifiedBasis, newDiagElementsClassified = ExpandBasis(eigvecFlow[i][sector], sector,
+                eigvalFlow[i][sector], numDiffFlow[i];
+                newClassifiedBasis=newClassifiedBasis, newDiagElementsClassified=newDiagElementsClassified)
         end
 
-        println("$i. Diagonalising matrix.")
-        # diagonalise this matrix, keeping only 'retainSize' 
-        # number of the new eigenstates.
-        spectrum = getSpectrum(basisStates, hamiltonianMatrix; maxNum=retainSize, keepfrom=keepfrom, tolerance=tolerance)
-
-        push!(spectrumFamily, spectrum)
-        @assert keys(basisStates) == keys(spectrum[1]) == keys(spectrum[2])
-        # create basis out of the eigenstates
-        basisStates = spectrum[2]
-
-        println("$i. Expanding basis.")
-        if i < length(numStatesFamily)
-            # expand the new basis to accomodate the new sites for the next step.
-            basisStates, diagElements = expandBasis(basisStates, numStatesFamily[i+1] - numStatesFamily[i], spectrum[1]; 
-                                                          occCriteria=occCriteria, magzCriteria=magzCriteria)
+        if i == length(hamFlow)
+            continue
         end
+        classifiedBasis, diagElementsClassified = TruncateBasis(newClassifiedBasis, newDiagElementsClassified, retainSize)
     end
-    return spectrumFamily
+    return eigvalFlow, eigvecFlow
 end
