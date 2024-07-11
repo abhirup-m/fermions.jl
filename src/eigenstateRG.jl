d1627d2c9ab3eac50179acb85d1db2f9f8b0753b
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-07-11 12:30:39 +0530

    fixed eigenstateRG according to recent changes

--- src/eigenstateRG.jl
+++ src/eigenstateRG.jl
@@ -1,20 +1,20 @@
-function getWavefunctionRG(initState::Dict{BitVector, Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; tolerance::Float64=1e-16)
-    
+function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; tolerance::Float64=1e-16)
+
     numEntangled = div(length(collect(keys(initState))[1]), 2)
-    stateFlowArray = Dict{BitVector, Float64}[]
+    stateFlowArray = Dict{BitVector,Float64}[]
     push!(stateFlowArray, initState)
-    
+
     pbar = Progress(numSteps; dt=0.1)
     for alpha in alphaValues
         newState = stateExpansionFunction(stateFlowArray[end])
         unitaryOperatorList = unitaryOperatorFunction(alpha, numEntangled, sectors)
         numEntangled = div(length(collect(keys(newState))[1]), 2)
-        stateRenormalisation = fetch.([Threads.@spawn ApplyOperator(newState, Dict(k => v)) for (k,v) in unitaryOperatorList])
+        stateRenormalisation = fetch.([Threads.@spawn ApplyOperator([operator], newState) for operator in unitaryOperatorList])
         mergewith!(+, newState, stateRenormalisation...)
 
-        newState= Dict(k => v for (k,v) in newState if abs(v) > tolerance)
+        newState = Dict(k => v for (k, v) in newState if abs(v) > tolerance)
         total_norm = sum(values(newState) .^ 2)^0.5
-        newState= Dict(k => v/total_norm for (k,v) in newState)
+        newState = Dict(k => v / total_norm for (k, v) in newState)
         push!(stateFlowArray, newState)
         next!(pbar)
     end
