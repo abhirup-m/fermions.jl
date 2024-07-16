4a930fa28aead441aa2da87c0d907087d6a4e76b
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-07-16 15:53:53 +0530

    moved unitaries and stateExpFuncs to lib

--- src/fermions.jl
+++ src/fermions.jl
@@ -7,6 +7,8 @@ include("base.jl")
 include("eigen.jl")
 include("correlations.jl")
 include("eigenstateRG.jl")
+include("reverseUnitaries.jl")
+include("stateExpansion.jl")
 include("iterativeDiag.jl")
 
 end # module fermions
