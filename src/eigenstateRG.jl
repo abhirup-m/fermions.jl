40804674c63cdaba60d58862bc415dad577a0a05
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-07-24 17:22:15 +0530

    added truncating mechanism for reverse RG

--- src/eigenstateRG.jl
+++ src/eigenstateRG.jl
@@ -1,19 +1,21 @@
-function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; tolerance::Float64=1e-16)
+function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; maxSize::Int64=0)
 
     @assert numSteps ≤ length(alphaValues)
     numEntangled = div(length(collect(keys(initState))[1]), 2)
     stateFlowArray = Dict{BitVector,Float64}[]
     push!(stateFlowArray, initState)
 
-    @showprogress for alpha in alphaValues[1:numSteps]
+    for alpha in alphaValues[1:numSteps]
         newState = stateExpansionFunction(stateFlowArray[end], sectors)
         unitaryOperatorList = unitaryOperatorFunction(alpha, numEntangled, sectors)
         numEntangled = div(length(collect(keys(newState))[1]), 2)
         mergewith!(+, newState, fetch.([Threads.@spawn ApplyOperator([operator], newState) for operator in unitaryOperatorList])...)
 
 
-        keepIndices = abs.(values(newState)) ./ maximum(abs.(values(newState))) .> tolerance
-        newState = Dict(collect(keys(newState))[keepIndices] .=> collect(values(newState))[keepIndices])
+        if maxSize < length(newState)
+            newState = Dict(sort(collect(newState), by=x->x|>last|>abs)[1:maxSize])
+            println("Drop ratio ~ ", round(sum(sort(collect(values(newState)))[maxSize:end] .^ 2) / sum(values(newState) .^ 2), sigdigits=1))
+        end
         total_norm = sum(values(newState) .^ 2)^0.5
         newState = Dict(keys(newState) .=> values(newState) ./ total_norm)
         push!(stateFlowArray, newState)
