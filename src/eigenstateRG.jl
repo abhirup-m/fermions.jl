2be2e83b19ed61364b7816e705b0314125a9172c
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-07-25 10:16:02 +0530

    reinstated tolerance for eigenstateRG

--- src/eigenstateRG.jl
+++ src/eigenstateRG.jl
@@ -1,4 +1,4 @@
-function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; maxSize::Int64=0)
+function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vector{Float64}, numSteps::Integer, unitaryOperatorFunction::Function, stateExpansionFunction::Function, sectors::String; maxSize::Int64=0, tolerance=1e-16)
 
     @assert numSteps ≤ length(alphaValues)
     numEntangled = div(length(collect(keys(initState))[1]), 2)
@@ -9,13 +9,13 @@ function getWavefunctionRG(initState::Dict{BitVector,Float64}, alphaValues::Vect
         newState = stateExpansionFunction(stateFlowArray[end], sectors)
         unitaryOperatorList = unitaryOperatorFunction(alpha, numEntangled, sectors)
         numEntangled = div(length(collect(keys(newState))[1]), 2)
-        mergewith!(+, newState, fetch.([Threads.@spawn ApplyOperator([operator], newState) for operator in unitaryOperatorList])...)
+        mergewith!(+, newState, fetch.([Threads.@spawn ApplyOperator([operator], newState; tolerance=tolerance) for operator in unitaryOperatorList])...)
 
 
         if maxSize < length(newState)
             println("Drop ratio ~ ", round(sum(sort(abs.(values(newState)))[maxSize:end] .^ 2) / sum(values(newState) .^ 2), sigdigits=1))
             newState = Dict(sort(collect(newState), by=x->x|>last|>abs)[1:maxSize])
-            println(newState)
+            println((minimum(abs.(values(newState))), sum(values(newState) .^ 2)))
         end
         total_norm = sum(values(newState) .^ 2)^0.5
         newState = Dict(keys(newState) .=> values(newState) ./ total_norm)
