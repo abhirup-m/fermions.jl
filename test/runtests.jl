4be7ff0c45851893ed08ec882c942b0eb20456d2
abhirup-m <mukherjeeabhirup44@gmail.com>
2024-07-28 21:10:19 +0530

    improved runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,7 @@
 using Test
 using LinearAlgebra, ProgressMeter
 
-include("../src/base.jl")
-include("../src/eigen.jl")
-include("../src/correlations.jl")
-include("../src/eigenstateRG.jl")
-include("../src/reverseUnitaries.jl")
-include("../src/stateExpansion.jl")
-include("../src/iterativeDiag.jl")
 include("testing_helpers.jl")
-
 include("base_tests.jl")
 include("eigen_tests.jl")
 include("correlation_tests.jl")
