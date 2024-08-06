8bb527a3ab11c6514c860aafb8614707e2365b75
Abhirup Mukherjee <mukherjeeabhirup44@gmail.com>
2024-08-06 11:24:59 +0530

    removed redundant functions

--- src/iterDiag.jl
+++ src/iterDiag.jl
@@ -1,55 +1,5 @@
 using Serialization, Random, LinearAlgebra, ProgressMeter
 
-"""Expands the basis to accomodate new 1-particle states by tacking 
-product states on to existing states. For eg. |10> + |01> -> (|10> + |01>)⊗|1>.
-"""
-function ExpandBasis(
-    basis::Vector{Dict{BitVector,Float64}},
-    sector::Tuple{Int64,Int64},
-    eigvals::Vector{Float64},
-    numNew::Int64;
-    totOccCriteria::Function=o -> true,
-    magzCriteria::Function=m -> true,
-    newClassifiedBasis::Dict{Tuple{Int64,Int64},Vector{Dict{BitVector,Float64}}}=Dict{Tuple{Int64,Int64},Vector{Dict{BitVector,Float64}}}(),
-    newDiagElementsClassified::Dict{Tuple{Int64,Int64},Vector{Float64}}=Dict{Tuple{Int64,Int64},Vector{Float64}}(),
-)
-    @assert length(basis) == length(eigvals)
-    newPairs = Tuple{NTuple{2 * numNew,Int64},Tuple{Int64,Int64}}[]
-    for newComb in collect(Iterators.product(repeat([[0, 1]], 2 * numNew)...))
-        newSector = sector .+ (sum(newComb),
-            sum(newComb[1:2:end]) - sum(newComb[2:2:end]))
-        if !(totOccCriteria(newSector[1]) && magzCriteria(newSector[2]))
-            continue
-        end
-        if newSector ∉ keys(newClassifiedBasis)
-            newClassifiedBasis[newSector] = []
-            newDiagElementsClassified[newSector] = []
-        end
-        push!(newPairs, (newComb, newSector))
-    end
-    Threads.@threads for (newComb, newSector) in newPairs
-        append!(newDiagElementsClassified[newSector], eigvals)
-        for stateDict in basis
-            newKeyValPairs = [(vcat(key, collect(newComb)), val) for (key, val) in stateDict]
-            push!(newClassifiedBasis[newSector], Dict(newKeyValPairs))
-        end
-    end
-    return newClassifiedBasis, newDiagElementsClassified
-end
-
-
-function TruncateBasis(newClassifiedBasis, newDiagElementsClassified, retainSize)
-    classifiedBasis = typeof(newClassifiedBasis)()
-    diagElementsClassified = Dict{keytype(classifiedBasis),Vector{Float64}}()
-    for (sector, basis) in newClassifiedBasis
-        sortperms = sortperm(newDiagElementsClassified[sector])
-        classifiedBasis[sector] = basis[sortperms][1:minimum((retainSize, length(basis)))]
-        diagElementsClassified[sector] = sort(newDiagElementsClassified[sector])[1:minimum((retainSize, length(newDiagElementsClassified[sector])))]
-    end
-    return classifiedBasis, diagElementsClassified
-end
-
-
 """Main function for iterative diagonalisation. Gives the approximate low-energy
 spectrum of a hamiltonian through the following algorithm: first diagonalises a
 Hamiltonian with few degrees of freedom, retains a fixed Ns number of low-energy
