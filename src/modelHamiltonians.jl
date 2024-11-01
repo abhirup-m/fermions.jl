7039287d3e9c9f4ec1b6e940e51514f7b0e42185
abhirup-m <mukherjeeabhirup44@gmail.com>
2024-11-01 23:55:28 +0530

    added k-space Kondo model to standard Hamiltonians

--- src/modelHamiltonians.jl
+++ src/modelHamiltonians.jl
@@ -33,3 +33,42 @@ function KondoModel(
     return hamiltonian
 end
 export KondoModel
+
+
+function KondoModel(
+        dispersion::Vector{Float64},
+        kondoJ::Float64;
+        globalField::Float64=0.,
+    )
+    numBathSites = length(dispersion)
+    hamiltonian = Tuple{String, Vector{Int64}, Float64}[]
+
+    # kinetic energy
+    for site in 1:(numBathSites-1)
+        push!(hamiltonian, ("n",  [1 + 2 * site], dispersion[site])) # up spin
+        push!(hamiltonian, ("n",  [2 + 2 * site], dispersion[site])) # down spin
+    end
+
+    # kondo terms
+    for indices in Iterators.product(1:numBathSites, 1:numBathSites)
+        up1, up2 = 2 .* indices .+ 1
+        down1, down2 = (up1, up2) .+ 1
+        push!(hamiltonian, ("n+-",  [1, up1, up2], kondoJ/4)) # n_{d up, n_{0 up}
+        push!(hamiltonian, ("n+-",  [1, down1, down2], -kondoJ/4)) # n_{d up, n_{0 down}
+        push!(hamiltonian, ("n+-",  [2, up1, up2], -kondoJ/4)) # n_{d down, n_{0 up}
+        push!(hamiltonian, ("n+-",  [2, down1, down2], kondoJ/4)) # n_{d down, n_{0 down}
+        push!(hamiltonian, ("+-+-",  [1, 2, down1, up2], kondoJ/2)) # S_d^+ S_0^-
+        push!(hamiltonian, ("+-+-",  [2, 1, up1, down2], kondoJ/2)) # S_d^- S_0^+
+    end
+
+    # global magnetic field (to lift any trivial degeneracy)
+    if globalField ≠ 0
+        for site in 0:numBathSites
+            push!(hamiltonian, ("n",  [1 + 2 * site], globalField))
+            push!(hamiltonian, ("n",  [2 + 2 * site], -globalField))
+        end
+    end
+
+    return hamiltonian
+end
+export KondoModel
