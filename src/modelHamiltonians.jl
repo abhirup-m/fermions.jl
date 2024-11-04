7d9a068eb2143df0737c7b7faf76dce4f08df50e
abhirup-m <mukherjeeabhirup44@gmail.com>
2024-11-04 11:19:54 +0530

    allowed cavities in k-space Kondo hamiltonian

--- src/modelHamiltonians.jl
+++ src/modelHamiltonians.jl
@@ -39,6 +39,7 @@ function KondoModel(
         dispersion::Vector{Float64},
         kondoJ::Float64;
         globalField::Float64=0.,
+        cavityIndices::Vector{Int64},
     )
     numBathSites = length(dispersion)
     hamiltonian = Tuple{String, Vector{Int64}, Float64}[]
@@ -51,14 +52,19 @@ function KondoModel(
 
     # kondo terms
     for indices in Iterators.product(1:numBathSites, 1:numBathSites)
+        if any(∈(cavityIndices), indices)
+            kondoJ_indices = 0
+        else
+            kondoJ_indices = kondoJ
+        end
         up1, up2 = 2 .* indices .+ 1
         down1, down2 = (up1, up2) .+ 1
-        push!(hamiltonian, ("n+-",  [1, up1, up2], kondoJ/4)) # n_{d up, n_{0 up}
-        push!(hamiltonian, ("n+-",  [1, down1, down2], -kondoJ/4)) # n_{d up, n_{0 down}
-        push!(hamiltonian, ("n+-",  [2, up1, up2], -kondoJ/4)) # n_{d down, n_{0 up}
-        push!(hamiltonian, ("n+-",  [2, down1, down2], kondoJ/4)) # n_{d down, n_{0 down}
-        push!(hamiltonian, ("+-+-",  [1, 2, down1, up2], kondoJ/2)) # S_d^+ S_0^-
-        push!(hamiltonian, ("+-+-",  [2, 1, up1, down2], kondoJ/2)) # S_d^- S_0^+
+        push!(hamiltonian, ("n+-",  [1, up1, up2], kondoJ_indices / 4)) # n_{d up, n_{0 up}
+        push!(hamiltonian, ("n+-",  [1, down1, down2], -kondoJ_indices / 4)) # n_{d up, n_{0 down}
+        push!(hamiltonian, ("n+-",  [2, up1, up2], -kondoJ_indices / 4)) # n_{d down, n_{0 up}
+        push!(hamiltonian, ("n+-",  [2, down1, down2], kondoJ_indices / 4)) # n_{d down, n_{0 down}
+        push!(hamiltonian, ("+-+-",  [1, 2, down1, up2], kondoJ_indices / 2)) # S_d^+ S_0^-
+        push!(hamiltonian, ("+-+-",  [2, 1, up1, down2], kondoJ_indices / 2)) # S_d^- S_0^+
     end
 
     # global magnetic field (to lift any trivial degeneracy)
